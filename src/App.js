0c5741d1eb29bfa10ba81ad5e364796efa583967
Maxime Baude <maxime.baude0@gmail.com>
2023-01-21 16:27:38 +0100

    App done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+
+import Square from "./Square";
+import Input from "./Input";
+
 function App() {
+
+  const [colorValue, setColorValue] = React.useState('')
+
   return (
     <div className="App">
-      
+      <Square colorValue={colorValue}/>
+      <Input 
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+      />
     </div>
   );
 }
